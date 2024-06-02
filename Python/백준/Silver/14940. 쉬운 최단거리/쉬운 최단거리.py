294198db227f6ead75194bb06cbd1d0d0e87e2f4
Mne <42082748+Mne-pr@users.noreply.github.com>
2024-06-02 14:40:09 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 500 ms, Memory: 40080 KB -BaekjoonHub

---
+++ Python/백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -0,0 +1,47 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def main():
+    N,M = map(int,input().split())
+    inpmap = []
+    visit = [[False for i in range(M)] for j in range(N)]
+    queue = deque()
+    
+    x = [0,0,1,-1]
+    y = [1,-1,0,0]
+    tarx, tary = 0, 0
+    
+    for i in range(N):
+        inp = list(map(int, input().split()))
+        inpmap.append(inp)
+        if 2 in inp:
+            tarx = inp.index(2)
+            tary = i
+    
+    queue.append([tary,tarx,1])
+    visit[tary][tarx] = True
+    inpmap[tary][tarx] = 0
+    
+    while queue:
+        tar = queue.popleft()
+        
+        for i in range(4):
+            nexty = tar[0]+y[i]
+            nextx = tar[1]+x[i]
+            if -1<nexty<N and -1<nextx< M and visit[nexty][nextx] == False:
+                if inpmap[nexty][nextx] != 0:
+                    visit[nexty][nextx] = True
+                    inpmap[nexty][nextx] = tar[2]
+                    queue.append([nexty,nextx,tar[2]+1])
+
+    for i in range(N):
+        for j in range(M):
+            if visit[i][j] == False and inpmap[i][j] == 1:
+                print(-1, end=' ')
+            else:
+                print(inpmap[i][j], end=' ')
+        print()
+        
+if __name__ == "__main__": 
+    main()
