6f7c85b043d4ea2cfce94e677d5a43610728ec74
Mne <42082748+Mne-pr@users.noreply.github.com>
2024-06-11 16:46:05 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -11,27 +11,18 @@ def main():
     
     for _ in range(N):
         land.append(list(map(int,input().rstrip())))   
-    q = deque([[0,0]])
-    
-    c = -1
-    n = len(q)
+    q = deque([[0,0,-1]])
     
     while q:
-        tn = 0
-        for _ in range(n):            
-            y,x = q.popleft()
-            land[y][x] = c
-        
-            for i in range(4):
-                nx, ny = x+xx[i], y+yy[i]
-                if nx == M-1 and ny == N-1:
-                    print((c-1)*(-1)); return
-                if 0 <= nx < M and 0 <= ny < N and [y,x] not in q and land[ny][nx] == 1:
-                    q.append([ny,nx])
-                    tn += 1   
-        n = tn
-        c -= 1
+        y,x,v = q.popleft()
+    
+        for i in range(4):
+            nx, ny = x+xx[i], y+yy[i]
+            if nx == M-1 and ny == N-1:
+                print((v-1)*(-1)); return
+            if 0 <= nx < M and 0 <= ny < N and land[ny][nx] == 1:
+                q.append([ny,nx,v-1])
+                land[ny][nx] = v-1
 
 if __name__ == "__main__":
     main()
-    
